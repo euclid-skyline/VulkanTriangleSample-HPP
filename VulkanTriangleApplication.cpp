ef1ec26d6a026d3e2bb1a4e38601487696073593
Euclid Skyline <euclid.skyline@gmail.com>
2025-08-30 19:09:28 +0300

    Create Vulkan Instance
    Updated includes for Vulkan and GLFW, removed redundancies.
    Changed `WIDTH` and `HEIGHT` to `constexpr`. Added
    `vk::raii::Context` and `vk::raii::Instance` for Vulkan
    resource management. Introduced `initWindow` and
    `createInstance` methods, with checks for required
    GLFW extensions in `createInstance`.

--- VulkanTriangleApplication.cpp
+++ VulkanTriangleApplication.cpp
@@ -1,18 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 #include <memory>
+
 #ifdef __INTELLISENSE__
 #include <vulkan/vulkan_raii.hpp>
 #else
 import vulkan_hpp;
 #endif
+
 #include <vulkan/vk_platform.h>
+
+#define GLFW_INCLUDE_VULKAN // REQUIRED only for GLFW CreateWindowSurface.
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-#include <stdexcept>
-#include <cstdlib>
-
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
@@ -26,6 +30,10 @@ public:
 private:
     GLFWwindow* window = nullptr;
 
+    vk::raii::Context  context;
+    vk::raii::Instance instance = nullptr;
+
+
     void initWindow() {
         glfwInit();
 
@@ -36,6 +44,7 @@ private:
     }
 
     void initVulkan() {
+        createInstance();
 
     }
 
@@ -49,6 +58,40 @@ private:
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+
+    void createInstance() {
+        constexpr vk::ApplicationInfo appInfo{ 
+            .pApplicationName = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName = "No Engine",
+            .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+            .apiVersion = vk::ApiVersion14 
+        };
+
+        // Get the required instance extensions from GLFW.
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // Check if the required GLFW extensions are supported by the Vulkan implementation.
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+        {
+            if (std::ranges::none_of(extensionProperties,
+                [glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+                { return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+            {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
+
+        vk::InstanceCreateInfo createInfo{ 
+            .pApplicationInfo = &appInfo,
+            .enabledExtensionCount = glfwExtensionCount,
+            .ppEnabledExtensionNames = glfwExtensions
+		};
+        instance = vk::raii::Instance(context, createInfo);
     }
 };
 
